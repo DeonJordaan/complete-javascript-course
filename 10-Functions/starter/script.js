86bb17c6e7063e35b7cd27afbad334b0b7c3995f
DeonJ76 <deon.jordaan76@gmail.com>
2021-03-16 15:31:13 +0200

    Section 10 complete

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -222,7 +222,8 @@
 // console.log(addVAT(23));
 
 // LECTURE 134 - CODING CHALLENGE
-
+/*
+// MY QUITE WRONG ANSWER 🤨
 const poll = {
 	question: 'What is your favorite programming language?',
 	options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
@@ -262,3 +263,142 @@ document.querySelector('.poll').addEventListener(
 	poll.registerNewAnswer.bind(poll)
 	// poll.displayResults.bind(poll)
 );
+*/
+// const poll = {
+// 	question: 'What is your favorite programming language?',
+// 	options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+// 	answers: new Array(4).fill(0),
+// 	registerNewAnswer() {
+// 		// Get the answer form the user
+// 		const answer = Number(
+// 			prompt(
+// 				`${this.question}\n${this.options.join(
+// 					'\n'
+// 				)}\n(Write option number)`
+// 			)
+// 		);
+// Register the answer received
+// Use of shortcircuiting to test the answer
+// 	typeof answer === 'number' &&
+// 		answer < this.answers.length &&
+// 		this.answers[answer]++;
+
+// 	this.displayResults();
+// 	this.displayResults('string');
+// },
+// Using ES6 to set the default parameter
+// 	displayResults(type = 'array') {
+// 		if (type === 'array') {
+// 			console.log(this.answers);
+// 		} else if (type === 'string') {
+// 			console.log(`Poll results are ${this.answers.join(', ')}`);
+// 		}
+// 	},
+// };
+// Got this section right! SMALL WINS!
+// document
+// 	.querySelector('.poll')
+// 	.addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+//Creating a new object(with {}) and inserting an array(with []) to use in the method call(displayResults)
+// poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+
+//////////////////////////////////
+// Lecture 135
+
+// const runOnce = function () {
+// 	console.log('This will never run again');
+// };
+// runOnce();
+
+// (function () {
+// 	console.log('This will never run again');
+// })();
+
+// (() => console.log('This will ALSO never run again'))();
+
+//////////////////////////////////
+// Lecture 136
+
+// const secureBooking = function () {
+// 	let passengerCount = 0;
+
+// 	return function () {
+// 		passengerCount++;
+// 		console.log(`${passengerCount} passengers`);
+// 	};
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+//////////////////////////////////
+// Lecture 137
+
+// Example 1
+// let f;
+
+// const g = function () {
+// 	const a = 23;
+// 	f = function () {
+// 		console.log(a * 2);
+// 	};
+// };
+
+// const h = function () {
+// 	const b = 777;
+// 	f = function () {
+// 		console.log(b * 2);
+// 	};
+// };
+// g();
+// f();
+// console.dir(f);
+
+// REassigned f function
+// h();
+// f();
+
+// console.dir(f);
+
+// Example 2
+
+// const boardPassenger = function (n, wait) {
+// 	const perGroup = n / 3;
+
+// 	setTimeout(function () {
+// 		console.log(`We are now boarding all ${n} passengers`);
+// 		console.log(`There are 3 groups, each with ${perGroup} passengers`);
+// 	}, wait * 1000);
+
+// 	console.log(`Will start baording in ${wait} seconds`);
+// };
+
+// const perGroup = 1000; // Proving that the closure has priority over the scope chain, this code does not affect the result of 'boardPassenger'
+// boardPassenger(180, 3);
+
+//////////////////////////////////
+// Lecture 138 - Coding Challenge
+
+// (function () {
+// 	const header = document.querySelector('h1');
+// 	header.style.color = 'red';
+// 	document.addEventListener('click', () => (header.style.color = 'blue'));
+// })();
+
+// Jonas Solution
+// (function () {
+// 	const header = document.querySelector('h1');
+// 	header.style.color = 'red';
+// 	document.querySelector('body').addEventListener('click', function () {
+// 		header.style.color = 'blue';
+// 	});
+// })();
