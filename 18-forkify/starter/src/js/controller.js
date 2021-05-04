ad046062a708e5bda0ab459a485548f78f57cf71
DeonJ76 <deon.jordaan76@gmail.com>
2021-05-04 21:06:41 +0200

    Section 18 project setup and refactoring for MVC architecture

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,13 +1,38 @@
-const recipeContainer = document.querySelector('.recipe');
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+const recipeContainer = document.querySelector('.recipe');
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+	try {
+		const id = window.location.hash.slice(1);
+		console.log(id);
+
+		if (!id) return; // Guard clause incase there is no id available
+		recipeView.renderSpinner();
+
+		// 1. Loading recipe
+		await model.loadRecipe(id);
+
+		// 2. Rendering recipe
+		recipeView.render(model.state.recipe);
+	} catch (err) {
+		alert(err);
+	}
+};
+
+// NOTE
+// Separate eventListeners for the same function
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
+//Combining them
+['hashchange', 'load'].forEach(event =>
+	window.addEventListener(event, controlRecipes)
+);
